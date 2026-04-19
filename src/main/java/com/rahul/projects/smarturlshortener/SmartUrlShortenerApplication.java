08b4aad3251927420cfeaf1a8097fe4349f24434
rahuldhiman18 <144884101+rahuldhiman18@users.noreply.github.com>
2026-04-19 10:36:10 +0530

    feat: project setup + first REST controller

---
+++ src/main/java/com/rahul/projects/smarturlshortener/SmartUrlShortenerApplication.java
@@ -0,0 +1,11 @@
+package com.rahul.projects.smarturlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartUrlShortenerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SmartUrlShortenerApplication.class, args);
+	}
+}
