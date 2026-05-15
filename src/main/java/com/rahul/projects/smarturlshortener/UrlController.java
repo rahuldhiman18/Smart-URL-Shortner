71c8daaf70304d20566d55a0857cf36220f83b19
rahuldhiman18 <144884101+rahuldhiman18@users.noreply.github.com>
2026-05-15 17:04:43 +0530

    feat: add crocs to handle server issue

--- src/main/java/com/rahul/projects/smarturlshortener/UrlController.java
+++ src/main/java/com/rahul/projects/smarturlshortener/UrlController.java
@@ -4,17 +4,19 @@ import java.net.URI;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 @RequestMapping("/api")
+
+@CrossOrigin(origins = {"http://localhost:5173","http://localhost:5174"})
 public class UrlController {
 
     private final UrlService urlService;
